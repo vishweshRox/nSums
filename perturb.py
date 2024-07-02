6440f02a7d1c1b5bdac2737555d1fa18192158ee
vishweshRox <vishwesh.palani@gmail.com>
2024-07-02 16:00:08 -0400

    file upload

---
+++ perturb.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 14:56:07 2024
+
+@author: vishweshpalani
+"""
+
+from polynomial import Polynomial
+
+"""
+NOTE: 
+    -> Coefficient arrays are represented in the form of arr[i] is the term x^i.
+    -> xCy is the number of ways to choose y objects from a collection of x objects.
+    
+This program uses perturbation theory to compute the coefficients for the polynomial that computes the sum of the first n powers of k.
+Coefficients are stored in pn_bank to memoize results.
+
+For example, the sum of the first n squares (k=2) can be computed by 0.5 * n^2 + 0.5 * n. 
+-> perturb returns a Polynomial object that wraps the coefficient array of the polynomial in question.
+So, perturb(2) will return Polynomial([0, 0.5, 0.5])
+
+    
+"""
+
+pn_bank = {}
+
+def comb(x, y):
+    #Computes xCy.
+    v = 1
+    for k in range(y):
+        v *= (x/y)
+        x -= 1
+        y -= 1
+    return int(v)
+
+def binom(n):
+    #Computes the polynomial coefficients for (x + 1)^n.
+    coef = []
+    for i in range(n + 1):
+        coef.append(comb(n, i))
+    return coef
+
+def perturb(n):
+    c_arr = pn_bank.get(n)
+    if c_arr:
+        return c_arr
+    coefs = binom(n + 1)
+    coefs[n + 1] = 0
+    rhs = Polynomial(1)
+    for i in range(0, n):
+        #Skips avoiding perturb(n + 1) and perturb(n) and triggering infinite recursion.
+        rhs += coefs[i] * perturb(i)
+    lhs = Polynomial(binom(n + 1)) - rhs
+    lhs *= (1/coefs[n])
+    return lhs
+
+if __name__ == '__main__':
+    for i in range(20):
+        print('POWER: ' + str(i))
+        p = perturb(i)
+        print('POLYNOMIAL: ' + str(p))
+        print()
+        print('SUM of 1st term: ' + str(p.compute(1)))
+        print('SUM of 2nd term: ' + str(p.compute(2)))
+        print('SUM of 3rd term: ' + str(p.compute(3)))
+        print('SUM of 10th term: ' + str(p.compute(10)))
+        print('-------------------------------------------------------')
+        print()
+
+
+
+
---
+++ polynomial.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 20:35:31 2024
+
+@author: vishweshpalani
+"""
+
+import numpy as np
+
+class Polynomial:
+    """
+    NOTE:
+        -> Constructor may accept a Polynomial, NumPy Array, List or Number. 
+        -> Empty iterables will be replaced with an object of an equivalent type holding the number 0, for ease of computation.
+    """
+    
+    def __init__(self, ipt=[]):
+        #arr is the coefficient array describing the polynomial.
+        if isinstance(ipt, Polynomial):
+            #Creates a copy of an existing polynomial.
+            ipt = ipt.getArr()
+        elif type(ipt) == np.ndarray:
+            if ipt.size == 0:
+                ipt += np.array([0])
+        elif type(ipt) == list:
+            if len(ipt) == 0:
+                ipt = [0]
+            ipt = np.array(ipt)
+        elif type(ipt) == int:
+            ipt = np.array([ipt])
+        else:
+            raise TypeError('Constructor must be a Polynomial, NumPy Array, List or Number.')
+        self.coef = np.array(ipt)
+        self.dim = self.coef.size - 1
+        
+    def __getitem__(self, idx):
+        return self.coef[idx]
+        
+    def __setitem__(self, n, coef):
+        self.coef[n] = coef
+        
+    def __iter__(self):
+        self.index = 0
+        return self
+    
+    def __next__(self):
+        if self.index <= self.dim:
+            coef = self[self.index]
+            self.index += 1
+            return coef
+        raise StopIteration
+    
+    def __repr__(self):
+        p_str = ''
+        first = True
+        for i in reversed(list(range(self.coef.size))):
+            if self[i] != 0 or i == 0:
+                p_str += ('+ ' if not first else '') + str(self[i]) + (f' * x^{str(i)} ' if i != 0 else '')
+                first = False
+            
+        return p_str
+    
+    def getArr(self):
+        return self.coef
+    
+    def getDim(self):
+        return self.dim
+    
+    def _expand(self, n):
+        #NOTE: Expansion will not affect the original array.
+        k = n - self.dim
+        exp_cpy = np.append(self.coef, np.zeros(k))
+        return Polynomial(exp_cpy)
+    
+    """
+    NOTE: Arithmetic operations accept constants and Polynomials only.
+    """
+        
+    def __add__(self, P2):
+        if not isinstance(P2, Polynomial):
+            P2 = Polynomial([P2])
+        if P2.getDim() < self.dim:
+            return P2 + self
+        else:
+            return Polynomial(self._expand(P2.getDim()).getArr() + P2.getArr())
+        
+    def __sub__(self, P2):
+        return self + (P2 * -1)
+        
+    def __mul__(self, P2):
+        if type(P2) in [int, float]:
+            #Wraps it with a Polynomial object.
+            P2 = Polynomial([P2])
+        elif P2.getDim() < 0:
+            P2 += Polynomial([0])
+        product = Polynomial([])
+        s1, s2 = self.dim + 1, P2.getDim() + 1
+        for i in range(s2):
+            temp = {}
+            p_e = P2[i]
+            for j in range(s1):
+                temp[i+j] = self[j] * p_e #Single pass per element.
+            new_p = Polynomial(np.zeros(max(temp.keys())+1))
+            for k in temp:
+                new_p[k] = temp[k]
+            product += new_p
+        return product
+    
+    def __rmul__(self, P2):
+        return self * P2
+    
+    def compute(self, x):
+        val = 0
+        for i in range(self.dim + 1):
+            val += self[i] * x ** i
+        return val
+    
+
+        
+    
+
+        
+    
+
+
+
